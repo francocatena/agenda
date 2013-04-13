2967dda1d033b2c30f66dd035906e1d5becaba8f
Franco Catena <francocatena@gmail.com>
2013-04-13 14:00:42 -0300

    Primera clase

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Agenda::Application.config.secret_key_base = 'a8eab07221ada02da282c9b090883692a95101e6a2f6926fa044c847e4a857d2a0c28fa2b0e7c7e67afd0c3ad1233429837e8d6bae02294032cc584c58330b4a'
